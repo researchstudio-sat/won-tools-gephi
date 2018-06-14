38c737c2885391beb61d3d532bef99db8fdeb3b0
Florian Kleedorfer <florian.kleedorfer@austria.fm>
2018-06-14 15:56:59 +0200

    Adapt script for shorter videos

--- src/main/python/create_video.py
+++ src/main/python/create_video.py
@@ -1,12 +1,12 @@
 execfile("../../DATA/DEV/workspace/won-tools-gephi/src/main/python/videomaker.py")
 videomaker(
-    ts_min=1493886171561, # "from" timestamp..
+    ts_min=1526560026841, # "from" timestamp..
     #ts_max=1518108181159, 
-  ts_max=1518108181159, # .."to" timestamp
-    frames=2000, # number of images in the video. eg 200 frames for a video at 20 frames per seconds = 10 seconds of video
+   ts_max=1528959731181, # .."to" timestamp
+    frames=200, # number of images in the video. eg 200 frames for a video at 20 frames per seconds = 10 seconds of video
   layout_iterations=2, #layout iterations between frames
   frames_before=50,
   frames_after=200,
-    output_prefix="../../DATA/DEV/workspace/gephi/video/generated_frames/frame_", # path where to write the png. images will be prefixed with "frame_" 
+    output_prefix="../../DATA/DEV/workspace/gephi/video1806/generated_frames/frame_", # path where to write the png. images will be prefixed with "frame_" 
     output_format=".png" # you probably want to leave png here
 )
